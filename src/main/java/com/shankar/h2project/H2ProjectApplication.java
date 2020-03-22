63db6723301f0dcd492e8aebdcadbfcff59ed15c
SPachari <shankar.pachari@gmail.com>
2020-03-21 19:41:58 -0700

    First version

---
+++ src/main/java/com/shankar/h2project/H2ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.shankar.h2project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shankar/h2project/H2ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shankar.h2project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
